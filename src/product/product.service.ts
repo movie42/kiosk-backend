d9d7e27702254ea0dfa97cf55fa0b214136c3c49
Jeong Seonkyo <39974627+tjsry0466@users.noreply.github.com>
2022-05-15 23:33:20 +0900

    가게 상품 조회 구현 (#20)
    * repository typeorm0.3에 맞게 변경
    
    * store->product 변경
    
    * 구현중
    
    * 가게 상품 조회 구현

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
 
+import { ProductRepository } from './repository/product.repository';
+
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(private readonly productRepository: ProductRepository) {}
+
+  async getStoreProductsByStoreId(storeId: number) {
+    return this.productRepository.getStoreProductsByStoreId(storeId);
+  }
+}
--- src/product/repository/product.repository.ts
+++ src/product/repository/product.repository.ts
@@ -7,4 +7,8 @@ import { Product } from '../entity/product.entity';
 @Injectable()
 export class ProductRepository {
   constructor(@InjectRepository(Product) private repository: Repository<Product>) {}
+
+  async getStoreProductsByStoreId(storeId: number) {
+    return this.repository.findBy({ storeId });
+  }
 }
