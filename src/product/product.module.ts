af679140dd7ff96fcfda0674c5137b9d9b3fa014
minsoo0715 <minsoo0715@naver.com>
2022-05-16 12:56:12 +0900

    fix: product Module에 option 엔티티 및 레포지토리 추가 - product.module.ts providers에 productOptionRepository 추가 - product.module.ts Typeorm imports에 Option entity 추가

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -1,13 +1,15 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { Option } from './entity/option.entity';
 import { Product } from './entity/product.entity';
 import { ProductResolver } from './product.resolver';
 import { ProductService } from './product.service';
+import { ProductOptionRepository } from './repository/product-option.repository';
 import { ProductRepository } from './repository/product.repository';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
-  providers: [ProductResolver, ProductService, ProductRepository],
+  imports: [TypeOrmModule.forFeature([Product, Option])],
+  providers: [ProductResolver, ProductService, ProductRepository, ProductOptionRepository],
 })
 export class ProductModule {}
