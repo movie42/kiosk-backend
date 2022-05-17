ed78700e9aaaac2a70899b679caaaa9eeead6c5a
Jeong Seonkyo <39974627+tjsry0466@users.noreply.github.com>
2022-05-17 22:46:03 +0900

    포트 변경 및 액션 이벤트 조건 변경 (#23)
    * 포트 변경
    
    * dev 세팅 변경
    
    * dd

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 
+import { isDev, isProd } from './common/constant';
+
 @Controller()
 export class AppController {
   @Get('health')
   health(): string {
-    return 'ok';
+    return isProd ? 'prod ok' : isDev ? 'dev ok' : 'ok';
   }
 }
