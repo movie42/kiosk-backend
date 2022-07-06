3208e76e3e7aaae220d762e2117a1b1ee63566ee
whiteB <whiteblock0715@gmail.com>
2022-07-06 15:34:30 +0900

    fix: order-status-default-ready migration 오류 수정
    - order-status-default-ready의 down 메소드에서 DROP이 아닌 원래 형태로 MODIFY를 하도록 수정

--- src/migration/1655730007497-order-status-default-ready.ts
+++ src/migration/1655730007497-order-status-default-ready.ts
@@ -5,11 +5,13 @@ export class orderStatusDefaultReady1655730007497 implements MigrationInterface 
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE \`order\` ADD \`status\` enum ('ready', 'done', 'complete', 'canceled') NOT NULL DEFAULT 'ready'`,
+      `ALTER TABLE \`order\` MODIFY \`status\` enum ('ready', 'done', 'complete', 'canceled') NOT NULL DEFAULT 'ready'`,
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`ALTER TABLE \`order\` DROP COLUMN \`status\``);
+    await queryRunner.query(
+      `ALTER TABLE \`order\` MODIFY \`status\` enum ('ready', 'done', 'complete', 'canceled') NOT NULL`,
+    );
   }
 }
