356719ba5bcbba90f27361166d993e6cf549b200
whiteB <whiteblock0715@gmail.com>
2022-07-06 15:08:50 +0900

    fix: updateOrderStatus 관련 오류 수정 - order Repository의 updateStatus가 boolean을 반환하도록 수정 - OrderStatusInput의 status 프로퍼티에 IsEnum() class-validator Decorator 추가

--- src/order/dto/order-status.input.ts
+++ src/order/dto/order-status.input.ts
@@ -1,9 +1,11 @@
 import { Field, InputType } from '@nestjs/graphql';
+import { IsEnum } from 'class-validator';
 
 import { OrderStatusType } from '../enum/order-status';
 
 @InputType()
 export class OrderStatusInput {
+  @IsEnum(OrderStatusType)
   @Field(() => OrderStatusType)
   status: OrderStatusType;
 }
