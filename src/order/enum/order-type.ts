d1bdca1e92df1d8f8932c8771e7f898211085d60
whiteB <whiteblock0715@gmail.com>
2022-07-08 12:24:10 +0900

    refactor: OrderType 문자로 작성
    - order-type.ts : OrderType 문자로 작성

--- src/order/enum/order-type.ts
+++ src/order/enum/order-type.ts
@@ -1,8 +1,8 @@
 import { registerEnumType } from '@nestjs/graphql';
 
 export enum OrderType {
-  HERE = 1,
-  GO = 2,
+  HERE = 'here',
+  GO = 'go',
 }
 
 registerEnumType(OrderType, { name: 'OrderType' });
